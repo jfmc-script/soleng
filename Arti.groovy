bea9adac5478cdd37582905c458996e6debc8f7f
mission-control <mission-control@mission-control-864786623-fx1jw>
2017-10-09 15:57:03 +0000

    File changed by admin

--- Arti.groovy
+++ Arti.groovy
@@ -15,3 +15,20 @@ artifactory(artifactoryToUserInput.name) {
     }
   }
 }
+
+artifactoryToUserInput2 = userInput (
+    name : "Input to name artifactory",
+    type : "ARTIFACTORY",
+    description : "please provide a to name artifactory"
+)
+artifactory(artifactoryToUserInput.name) {
+  localRepository("local-repository-key") {
+    replication(artifactoryToUserInput) {
+      username artifactoryToUserInput.credentials.userName
+      password artifactoryToUserInput.credentials.password
+
+      cronExp "0 0/9 14 * * ?"
+      socketTimeoutMillis 15000
+    }
+  }
+}
